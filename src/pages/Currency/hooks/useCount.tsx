5ddc9825efc7af8b2a31691057a121850e1b8802
YigitaliyevIslomjon <77922591+YigitaliyevIslomjon@users.noreply.github.com>
2022-12-03 10:56:20 +0500

    last changes

--- src/pages/Currency/hooks/useCount.tsx
+++ src/pages/Currency/hooks/useCount.tsx
@@ -1,13 +1,10 @@
-import { useState } from "react";
+import { UIEvent, useState } from "react";
 import { Currency } from "../../../types/Currency.types";
 
 export const useCount = () => {
   const [count, setCount] = useState<number>(7);
 
-  const handleScroll = (
-    event: React.UIEvent<HTMLElement>,
-    data: Currency[]
-  ) => {
+  const handleScroll = (event: UIEvent<HTMLElement>, data: Currency[]) => {
     let element = event.currentTarget;
     if (
       element.scrollHeight - element.scrollTop === element.clientHeight &&
