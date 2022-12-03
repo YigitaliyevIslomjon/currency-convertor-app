626b2aae32b9032ebb8ecedcb0a8db7c2e1e8fe5
YigitaliyevIslomjon <77922591+YigitaliyevIslomjon@users.noreply.github.com>
2022-12-03 09:14:48 +0500

    label qo'shildi convertlar uchun

--- src/pages/CurrencyConvertor/index.tsx
+++ src/pages/CurrencyConvertor/index.tsx
@@ -1,4 +1,4 @@
-import { Button, Input, Select } from "antd";
+import { Button, Input, Select, Typography } from "antd";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faRightLeft } from "@fortawesome/free-solid-svg-icons";
 import Spinner from "../../components/Spinner";
@@ -33,38 +33,58 @@ function CurrencyConvertor() {
   return (
     <div className="currency">
       <div className="currency__container">
-        <Input
-          value={inputAmunt}
-          size="large"
-          className="currency_width"
-          onChange={handleAmountChange}
-        />
-        <Select
-          size="large"
-          value={fromCurrency}
-          className="currency_width"
-          onChange={fromCurrencyChange}
-          options={currencyList.map((currency: Currency) => ({
-            label: currency.name,
-            value: currency.name,
-          }))}
-        />
-        <div className="currency__circule" onClick={repalceCurrencyVal}>
-          <FontAwesomeIcon icon={faRightLeft} />
+        <div className="currency__formControl">
+          <label className="currency_label">Amount</label>
+          <Input
+            value={inputAmunt}
+            size="large"
+            className="currency_width"
+            onChange={handleAmountChange}
+          />
         </div>
-        <Select
-          size="large"
-          value={toCurrency}
-          className="currency_width"
-          onChange={toCurrencyChange}
-          options={currencyList.map((currency) => ({
-            label: currency.name,
-            value: currency.name,
-          }))}
-        />
+        <div className="currency__formControl">
+          <label className="currency_label">From</label>
+          <Select
+            size="large"
+            value={fromCurrency}
+            className="currency_width"
+            onChange={fromCurrencyChange}
+            options={currencyList.map((currency: Currency) => ({
+              label: currency.name,
+              value: currency.name,
+            }))}
+          />
+        </div>
+        <div className="currency__circule" onClick={repalceCurrencyVal}>
+          <FontAwesomeIcon color="#3F90E8" icon={faRightLeft} />
+        </div>
+        <div className="currency__formControl">
+          <label className="currency_label">To</label>
+          <Select
+            size="large"
+            value={toCurrency}
+            className="currency_width"
+            onChange={toCurrencyChange}
+            options={currencyList.map((currency) => ({
+              label: currency.name,
+              value: currency.name,
+            }))}
+          />
+        </div>
       </div>
       <div className="currency__container">
-        <Input value={convetResult} size="large" className="currency_width" />
+        <div className="currency__formControl">
+          <label className="currency_label">
+            {inputAmunt +
+              "  " +
+              fromCurrency +
+              " = " +
+              convetResult +
+              " " +
+              toCurrency}
+          </label>
+          <Input value={convetResult} size="large" className="currency_width" />
+        </div>
         <Button type="primary" size="large" onClick={handleConvertChange}>
           Convert
         </Button>
